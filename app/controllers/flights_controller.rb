52ea10ee0945590a30e842e489e763f5bea3cb5d
Luke Hammer <textchimp@gmail.com>
2022-09-25 22:24:39 +1000

    init commit

---
+++ app/controllers/flights_controller.rb
@@ -0,0 +1,28 @@
+class FlightsController < ApplicationController
+
+  def index
+    render json: Flight.all
+  end # index
+
+
+  def search
+
+    flights = Flight.where origin: params[:origin], destination: params[:destination]
+
+    # sleep 3  # test the loading message in the frontend
+
+    render json: flights, include: {
+      airplane: {
+        only: [ :name ]
+      }
+    }
+
+  end # search
+
+
+  def show
+    flight = Flight.find params[:id]
+    render json: flight
+  end # show
+
+end
